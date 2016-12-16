86d8107e3953b9297eccb79ab2f70ea5af68ef36
Tad Park <tadp@users.noreply.github.com>
2016-12-16 06:05:02 -0800

    Gems: Prefer sidekiq over delayed_job

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module BlankRails
       generate.view_specs false
     end
     config.action_controller.action_on_unpermitted_parameters = :raise
-    config.active_job.queue_adapter = :delayed_job
+    config.active_job.queue_adapter = :sidekiq
   end
 end
